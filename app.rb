dd02b38be8d2fadc36c4bb34a986b8f12cb2bd38
github username <github email address>
2020-05-26 21:56:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
+require 'securerandom'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+  end 
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    # binding.pry
+    @session = session
+    session[:item] = params[:item]
+
+    erb :show
+  end 
+
 end
