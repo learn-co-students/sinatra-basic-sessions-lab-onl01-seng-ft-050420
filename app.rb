ceddc336b881b8f9c9b9141eaafd6cc11677f08f
Luke Chen <luke.chen.2010@gmail.com>
2020-05-29 19:52:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
 class App < Sinatra::Base
+  get '/' do
+    @session = session
+    erb :index
+  end
+  
+  post '/checkout' do
+    @item = params[:item]
+    session[:item] = @item
+    @session = session
+    
+    return @item
+  end
 end
